b90aa89b434b0bcc104177e925b676a8b0d35828
johannes.wijaya <johannes.wijaya@idexpress.com>
2024-03-07 17:56:35 +0700

    add loop + sample test cases

---
+++ test/all_test.go
@@ -0,0 +1,40 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/go-kit/kit/endpoint"
+	"github.com/louvri/gokrt/after_async"
+)
+
+type key int
+
+var test key = 1
+
+func TestAfterAsync(t *testing.T) {
+	ctx := context.WithValue(context.Background(), test, 1)
+	response, err := endpoint.Chain(after_async.Middleware(
+		func(ctx context.Context, req interface{}) (interface{}, error) {
+			fmt.Printf("HELLOOO %v\n", ctx.Value(test))
+			fmt.Println(req)
+			time.Sleep(1000)
+			return nil, nil
+		},
+		func(data interface{}, err error) interface{} {
+			return data
+		},
+	))(func(ctx context.Context, req interface{}) (interface{}, error) {
+		a := 0
+		for i := 0; i < 10000; i++ {
+			a++
+		}
+		return a, nil
+	})(ctx, 3)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	fmt.Println(response)
+}
