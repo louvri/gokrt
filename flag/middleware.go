e2846f5cefc3bf7f55c1910ade349492a8fcd754
johannes.wijaya <johannes.wijaya@idexpress.com>
2024-03-19 14:46:31 +0700

    remove reset param at flag middleware

--- flag/middleware.go
+++ flag/middleware.go
@@ -10,10 +10,10 @@ import (
 	goRedis "github.com/redis/go-redis/v9"
 )
 
-func Middleware(key, field string, value interface{}, duration time.Duration, redis *goRedis.Client, reset ...bool) endpoint.Middleware {
+func Middleware(key, field string, value interface{}, duration time.Duration, redis *goRedis.Client) endpoint.Middleware {
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, req interface{}) (interface{}, error) {
-			if len(reset) > 0 && reset[0] {
+			if value == nil {
 				cmd := redis.HDel(ctx, key, field)
 				if cmd.Err() != nil {
 					return nil, cmd.Err()
@@ -31,7 +31,6 @@ func Middleware(key, field string, value interface{}, duration time.Duration, re
 					ctx = context.WithValue(ctx, sys_key.EOF, "eof")
 					return next(ctx, nil)
 				}
-
 			}
 			return next(ctx, req)
 		}
