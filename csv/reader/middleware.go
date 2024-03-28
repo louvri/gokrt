55e0c41f5dc8635722964c63152ea4465b4b5bb2
johannes.wijaya <johannes.wijaya@idexpress.com>
2024-03-28 13:37:25 +0700

    csv_reader to return error at the end of the call (eventhough it's ignored during the execution)

--- csv/reader/middleware.go
+++ csv/reader/middleware.go
@@ -3,6 +3,7 @@ package reader
 import (
 	"bufio"
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"strings"
@@ -74,6 +75,7 @@ func Middleware(filename string, size int, decoder func(data interface{}) interf
 			}
 			var err error
 			var response interface{}
+			nextErr := make([]error, 0)
 			lineNumber := 1
 			for scanner.Scan() {
 				text := scanner.Text()
@@ -99,6 +101,8 @@ func Middleware(filename string, size int, decoder func(data interface{}) interf
 						response, err = exec(ctx, data, false)
 						if err != nil && !ignoreError {
 							return nil, fmt.Errorf("%s:%s", "csv_reader_middleware:", err.Error())
+						} else if err != nil {
+							nextErr = append(nextErr, err)
 						}
 					}
 					ctx = context.WithValue(ctx, sys_key.SOF, false)
@@ -108,8 +112,13 @@ func Middleware(filename string, size int, decoder func(data interface{}) interf
 			}
 			if tmp, err := exec(ctx, nil, true); err != nil && !ignoreError {
 				return nil, fmt.Errorf("%s:%s", "csv_reader_middleware:", err.Error())
-			} else if tmp != nil {
-				response = tmp
+			} else {
+				if err != nil {
+					nextErr = append(nextErr, err)
+				}
+				if tmp != nil {
+					response = tmp
+				}
 			}
 			if err := scanner.Err(); err != nil && !ignoreError {
 				return nil, fmt.Errorf("%s:%s", "csv_reader_middleware:", err.Error())
@@ -119,6 +128,19 @@ func Middleware(filename string, size int, decoder func(data interface{}) interf
 				return nil, fmt.Errorf("%s:%s", "csv_reader_middleware:", err.Error())
 			} else if tmp != nil {
 				response = tmp
+			}
+			if len(nextErr) > 0 {
+				duplicate := make(map[string]string)
+				allErrors := ""
+				for _, err := range nextErr {
+					errString := err.Error()
+					if duplicate[errString] == "" && errString != "" {
+						duplicate[errString] = errString
+						allErrors = allErrors + errString
+					}
+
+				}
+				return response, errors.New(allErrors)
 			}
 			return response, nil
 		}
