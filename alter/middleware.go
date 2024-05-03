73f27db58fce0bb90a3fd84b5b47425b3f374fb5
johannes.wijaya <johannes.wijaya@idexpress.com>
2024-05-03 17:06:08 +0700

    add feature no wait at wait and alter middleware

--- alter/middleware.go
+++ alter/middleware.go
@@ -10,22 +10,30 @@ import (
 func Middleware(
 	e endpoint.Endpoint,
 	preprocessor func(data interface{}, err error) interface{},
-	postprocessor func(original interface{}, data interface{}, err error) (interface{}, error)) endpoint.Middleware {
+	postprocessor func(original interface{}, data interface{}, err error) (interface{}, error),
+	wait ...bool) endpoint.Middleware {
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, req interface{}) (interface{}, error) {
 			original, err := next(ctx, req)
 			if original != nil || err != nil {
 				result := preprocessor(original, err)
 				if result != nil {
-					var wg sync.WaitGroup
-					var altered interface{}
-					wg.Add(1)
-					go func() {
-						defer wg.Done()
-						altered, err = e(ctx, result)
-					}()
-					wg.Wait()
-					return postprocessor(original, altered, err)
+					nwait := len(wait)
+					if nwait > 0 && wait[0] {
+						var wg sync.WaitGroup
+						var altered interface{}
+						wg.Add(1)
+						go func() {
+							defer wg.Done()
+							altered, err = e(ctx, result)
+						}()
+						wg.Wait()
+						return postprocessor(original, altered, err)
+					} else {
+						altered, err := e(ctx, result)
+						return postprocessor(original, altered, err)
+					}
+
 				} else {
 					return nil, nil
 				}
