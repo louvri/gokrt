3cd6dfc6bb28dfe11848ef2c7b3101d28a92fb15
johannes.wijaya <johannes.wijaya@idexpress.com>
2024-05-13 10:35:47 +0700

    on eof with custom condition

---
+++ on_eof_custom/middleware.go
@@ -0,0 +1,28 @@
+package on_eof
+
+import (
+	"context"
+
+	"github.com/go-kit/kit/endpoint"
+	"github.com/louvri/gokrt/sys_key"
+)
+
+func Middleware(condition func(ctx context.Context) bool, middlewares ...endpoint.Middleware) endpoint.Middleware {
+	return func(next endpoint.Endpoint) endpoint.Endpoint {
+		return func(ctx context.Context, req interface{}) (interface{}, error) {
+			eof := ctx.Value(sys_key.EOF)
+			if eof != nil && eof == "eof" && condition(ctx) {
+				next = func(ctx context.Context, req interface{}) (interface{}, error) {
+					return "", nil
+				}
+				for i := len(middlewares) - 1; i >= 0; i-- { // reverse
+					next = middlewares[i](next)
+				}
+				return next(ctx, req)
+			} else if eof != nil && eof == "err" {
+				return nil, nil
+			}
+			return next(ctx, req)
+		}
+	}
+}
