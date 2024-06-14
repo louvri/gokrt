9251df5c88afe56d7df697c6fd1a5bfbe5e54cd3
johannes.wijaya <johannes.wijaya@idexpress.com>
2024-06-14 17:55:36 +0700

    add new middleware before

---
+++ before/middleware.go
@@ -0,0 +1,36 @@
+package before
+
+import (
+	"context"
+	"sync"
+
+	"github.com/go-kit/kit/endpoint"
+	icontext "github.com/louvri/gokrt/icontext"
+)
+
+func Middleware(e endpoint.Endpoint, preprocessor func(data interface{}) interface{}, wait ...bool) endpoint.Middleware {
+	return func(next endpoint.Endpoint) endpoint.Endpoint {
+		return func(ctx context.Context, req interface{}) (interface{}, error) {
+			result := preprocessor(req)
+			if result != nil {
+				nwait := len(wait)
+				if nwait > 0 && wait[0] {
+					var wg sync.WaitGroup
+					wg.Add(1)
+					go func() {
+						e(ctx, result)
+						wg.Done()
+					}()
+					wg.Wait()
+				} else {
+					if _, ok := ctx.(*icontext.CopyContext); !ok {
+						ctx = icontext.New(ctx)
+					}
+					go e(ctx, result)
+				}
+			}
+
+			return next(ctx, req)
+		}
+	}
+}
