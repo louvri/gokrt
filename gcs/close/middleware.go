2dc910058d64293d408fe051e1560b0383bc500a
johannes.wijaya <johannes.wijaya@idexpress.com>
2024-08-28 13:20:54 +0700

    bugfix gcs close

--- gcs/close/middleware.go
+++ gcs/close/middleware.go
@@ -12,7 +12,7 @@ func Middleware() endpoint.Middleware {
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, req interface{}) (interface{}, error) {
 			eof := ctx.Value(sys_key.EOF)
-			if eof != nil && eof != "" {
+			if eof != nil && eof == "eof" {
 				if fileObjects, ok := ctx.Value(sys_key.FILE_OBJECT_KEY).(map[string]interface{}); ok {
 					for _, fileObject := range fileObjects {
 						if con, ok := fileObject.(connection.Connection); ok && con.Driver() == "gcs" {
