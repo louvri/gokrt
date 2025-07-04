27ab9e0e2a343a971e1f5a33d749f4490f4482fd
claudio.vivaldi <claudio.vivaldi@idexpress.com>
2025-07-04 14:46:08 +0700

    solve on alter

--- alter/middleware_test.go
+++ alter/middleware_test.go
@@ -3,14 +3,21 @@ package alter_test
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log"
 	"testing"
+	"time"
 
 	"github.com/go-kit/kit/endpoint"
 	"github.com/louvri/gokrt/after"
 	"github.com/louvri/gokrt/alter"
 	RUN_WITH_OPTION "github.com/louvri/gokrt/option"
+	"github.com/louvri/gosl"
 )
+
+type TestKey int
+
+var TKey TestKey = 13
 
 type Mock interface {
 	Main(ctx context.Context, request interface{}) (interface{}, error)
@@ -211,4 +218,194 @@ func TestAlterAfter(t *testing.T) {
 		t.Log("should error")
 		t.FailNow()
 	}
+}
+
+func TestWithMultipleValueContext(t *testing.T) {
+	m := NewMock()
+	ctx := context.WithValue(context.Background(),
+		gosl.SQL_KEY,
+		gosl.NewQueryable(gosl.ConnectToDB(
+			"root",
+			"abcd",
+			"localhost",
+			"3306",
+			"testTx",
+			1,
+			1,
+			2*time.Minute,
+			2*time.Minute,
+		)))
+	ctx = context.WithValue(ctx,
+		TKey,
+		gosl.NewQueryable(gosl.ConnectToDB(
+			"root",
+			"abcd",
+			"localhost",
+			"3306",
+			"testTx2",
+			1,
+			1,
+			2*time.Minute,
+			2*time.Minute,
+		)))
+	resp, err := endpoint.Chain(
+		alter.Middleware(m.First, func(data interface{}, err error) interface{} {
+			return data
+		}, func(original, data interface{}, err error) (interface{}, error) {
+			return data, err
+		}),
+		alter.Middleware(ep, func(data interface{}, err error) interface{} {
+			return data
+		}, func(original, data interface{}, err error) (interface{}, error) {
+			return data, err
+		}),
+	)(m.Main)(ctx, "request1")
+	if err != nil {
+		t.Log(err.Error())
+		t.FailNow()
+	}
+	if result, ok := resp.(string); ok {
+		if result != "first endpoint" {
+			t.Logf("got '%s' expected 'first endpoint'", result)
+			t.FailNow()
+		}
+	}
+}
+
+func ep(ctx context.Context, req interface{}) (interface{}, error) {
+	ctx, kit := gosl.New(ctx)
+
+	_, ok := ctx.Value(gosl.SQL_KEY).(*gosl.Queryable)
+	if !ok {
+		return nil, errors.New("sql not initiated")
+	}
+
+	err := kit.RunInTransaction(
+		ctx,
+		func(ctx context.Context) error {
+			err := svc1(ctx)
+			if err != nil {
+				return err
+			}
+
+			err = svc2(ctx)
+			if err != nil {
+				return err
+			}
+			return err
+		},
+	)
+	return "done ep", err
+}
+
+func svc1(ctx context.Context) error {
+	ctx, kit := gosl.New(ctx)
+	err := kit.RunInTransaction(ctx, func(ctx context.Context) error {
+		if err := kit.ContextSwitch(ctx, TKey); err == nil {
+			err = repo(ctx, "SWITCHsatutigabelas1")
+			if err != nil {
+				return err
+			}
+		}
+		if err := kit.ContextReset(ctx); err == nil {
+			err = repo(ctx, "RESETsatutigabelas1")
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	return err
+}
+
+func repo(ctx context.Context, data string) error {
+	ctx, kit := gosl.New(ctx)
+	err := kit.RunInTransaction(ctx, func(ctx context.Context) error {
+		var queryable *gosl.Queryable
+		ictx, ok := ctx.Value(gosl.INTERNAL_CONTEXT).(*gosl.InternalContext)
+		if ok {
+			queryable = ictx.Get(gosl.SQL_KEY).(*gosl.Queryable)
+		} else {
+			ref := ctx.Value(gosl.SQL_KEY)
+			if ref == nil {
+				err := errors.New("database is not initialized")
+				return err
+			}
+			queryable = ref.(*gosl.Queryable)
+		}
+		_, err := queryable.ExecContext(ctx, fmt.Sprintf("INSERT INTO `hello_1` (data) VALUES('%s')", data))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func svc2(ctx context.Context) error {
+	ctx, kit := gosl.New(ctx)
+	err := kit.RunInTransaction(ctx, func(ctx context.Context) error {
+		if err := kit.ContextSwitch(ctx, TKey); err == nil {
+			err = repo(ctx, "SWITCHsatutigabelas2")
+			if err != nil {
+				return nil
+			}
+
+		}
+		if err := kit.ContextReset(ctx); err == nil {
+			err = repo(ctx, "RESETsatutigabelas2")
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	return err
+}
+
+func reset(ctx context.Context) error {
+	ctx, kit := gosl.New(ctx)
+	var queryable *gosl.Queryable
+	queryable, ok := ctx.Value(gosl.SQL_KEY).(*gosl.Queryable)
+	if !ok {
+		return errors.New("sql not initiated")
+	}
+	_, err := queryable.ExecContext(ctx, "DELETE FROM `hello_1`")
+	if err != nil {
+		return err
+	}
+	_, err = queryable.ExecContext(ctx, "DELETE FROM `hello_2`")
+	if err != nil {
+		return err
+	}
+
+	if err = kit.ContextSwitch(ctx, TKey); err == nil {
+		var queryable *gosl.Queryable
+
+		ictx, ok := ctx.Value(gosl.INTERNAL_CONTEXT).(*gosl.InternalContext)
+		if ok {
+			queryable = ictx.Get(gosl.SQL_KEY).(*gosl.Queryable)
+			ctx = ictx.Base()
+		} else {
+			ref := ctx.Value(gosl.SQL_KEY)
+			if ref == nil {
+				err = errors.New("database is not initialized")
+				return err
+			}
+			queryable = ref.(*gosl.Queryable)
+		}
+
+		_, err := queryable.ExecContext(ctx, "DELETE FROM `hello_1`")
+		if err != nil {
+			return err
+		}
+		_, err = queryable.ExecContext(ctx, "DELETE FROM `hello_2`")
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
